00e82a7dbf2ac0c52cdba75540b4d6b528247965
Bartosz Kaszowski <bartosz@kaszowski.eu>
2021-01-29 07:46:20 +0100

    Utworzenie layoutu video.game - bez responsywnosci pojedynczej strony gry

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Laracasts Video Game</title>
+</head>
+
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+            <div class="flex flex-col lg:flex-row items-center">
+                <a href="/">
+                    <img src="https://assets.laracasts.com/images/negative-logo.svg" alt="laracasts"
+                        class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-0 lg:ml-16 space-x-8 mt-6 lg:mt-0">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center mt-6 lg:mt-0">
+                <div class="relative">
+                    <input type="text"
+                        class="bg-gray-800 text-sm rounded-full focus:outline-none focus:ring focus:border-blue-300 w-64 px-3 pl-8 py-1"
+                        placeholder="Szukaj ...">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+
+                        <svg class="fill-current text-gray-400 w-4" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24">
+                            <path
+                                d="M21.172 24l-7.387-7.387c-1.388.874-3.024 1.387-4.785 1.387-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9c0 1.761-.514 3.398-1.387 4.785l7.387 7.387-2.828 2.828zm-12.172-8c3.859 0 7-3.14 7-7s-3.141-7-7-7-7 3.14-7 7 3.141 7 7 7z" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="https://assets.laracasts.com/images/mascot/larydefault.svg" alt="avatar"
+                            class="rounded-full w-8"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered by <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
+</body>
+
+</html>
